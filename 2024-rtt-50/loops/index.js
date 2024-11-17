a0b86812b76d1f9abd08647c731b4f652efbf5c2
younesbahou123 <myeurikateam@gmail.com>
2024-11-17 02:13:58 -0500

    second problem

--- 2024-rtt-50/loops/index.js
+++ 2024-rtt-50/loops/index.js
@@ -14,3 +14,24 @@ for(i=1 ;i<=100 ; i++){
     }
     
 }
+
+// Declare an arbitrary number n
+let n = 13; // You can set this to any starting value
+
+// Function to check if a number is prime
+function isPrime(num) {
+  if (num <= 1) return false; // 0 and 1 are not prime
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) return false; // Not a prime number
+  }
+  return true; // Prime number
+}
+
+// Loop to find the next prime number starting from n
+while (true) {
+  n++; // Increment n
+  if (isPrime(n)) {
+    console.log(`The next prime number is: ${n}`);
+    break; // Exit the loop as soon as a prime is found
+  }
+}
--- 2024-rtt-50/loops/tempCodeRunnerFile.js
+++ 2024-rtt-50/loops/tempCodeRunnerFile.js
@@ -1,5 +1,20 @@
-let x=10;
-while (x>=0){
-    x--;
-    console.log(x);
+// Declare an arbitrary number n
+let n = 13; // You can set this to any starting value
+
+// Function to check if a number is prime
+function isPrime(num) {
+  if (num <= 1) return false; // 0 and 1 are not prime
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) return false; // Not a prime number
+  }
+  return true; // Prime number
+}
+
+// Loop to find the next prime number starting from n
+while (true) {
+  n++; // Increment n
+  if (isPrime(n)) {
+    console.log(`The next prime number is: ${n}`);
+    break; // Exit the loop as soon as a prime is found
+  }
 }
